22ab8793eb7131849d5bb595593f597f2e41aaa4
Sarvar Khalimov <khalimovsarvar@gmail.com>
2023-06-07 18:39:34 +0500

    Implement Login servlet and JDBC parts

--- src/main/java/sarvar/group/controllerServlet/Register.java
+++ src/main/java/sarvar/group/controllerServlet/Register.java
@@ -1,6 +1,7 @@
 package sarvar.group.controllerServlet;
 
 import lombok.SneakyThrows;
+import sarvar.group.controllerServlet.util.Response;
 import sarvar.group.modelDao.User;
 import sarvar.group.service.DBConnection;
 import sarvar.group.service.util.Result;
@@ -77,64 +78,6 @@ public class Register extends HttpServlet {
 
         DBConnection dbConnection = new DBConnection();
         Result result = dbConnection.register(user);
-        if (result.isSuccess()) {
-            printWriter.write(
-                    "<!DOCTYPE html>\n" +
-                            "<html lang=\"en\">\n" +
-                            "<head>\n" +
-                            "    <meta charset=\"UTF-8\">\n" +
-                            "    <title>Title</title>\n" +
-                            "    <style type=\"text/css\">\n" +
-                            "        @import url('//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css');\n" +
-                            "\n" +
-                            "        .success-msg{\n" +
-                            "            margin: 10px 0;\n" +
-                            "            padding: 10px;\n" +
-                            "            border-radius: 3px 3px 3px 3px;\n" +
-                            "        }\n" +
-                            "        .success-msg {\n" +
-                            "            color: #270;\n" +
-                            "            background-color: #DFF2BF;\n" +
-                            "        }\n" +
-                            "    </style>\n" +
-                            "</head>\n" +
-                            "<body>\n" +
-                            "    <div class=\"success-msg\">\n" +
-                            "        <i class=\"fa fa-check\"></i>\n" +
-                            result.getMessage() +
-                            "    </div>\n" +
-                            "</body>\n" +
-                            "</html>"
-            );
-        } else {
-            printWriter.write(
-                    "<!DOCTYPE html>\n" +
-                            "<html lang=\"en\">\n" +
-                            "<head>\n" +
-                            "    <meta charset=\"UTF-8\">\n" +
-                            "    <title>Title</title>\n" +
-                            "    <style type=\"text/css\">\n" +
-                            "        @import url('//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css');\n" +
-                            "\n" +
-                            "        .warning-msg {\n" +
-                            "            margin: 10px 0;\n" +
-                            "            padding: 10px;\n" +
-                            "            border-radius: 3px 3px 3px 3px;\n" +
-                            "        }\n" +
-                            "        .warning-msg {\n" +
-                            "            color: #9F6000;\n" +
-                            "            background-color: #FEEFB3;\n" +
-                            "        }\n" +
-                            "    </style>\n" +
-                            "</head>\n" +
-                            "<body>\n" +
-                            "    <div class=\"warning-msg\">\n" +
-                            "        <i class=\"fa fa-warning\"></i>\n" +
-                            result.getMessage() +
-                            "    </div>\n" +
-                            "</body>\n" +
-                            "</html>"
-            );
-        }
+        Response.respond(printWriter, result);
     }
 }
--- src/main/java/sarvar/group/service/DBConnection.java
+++ src/main/java/sarvar/group/service/DBConnection.java
@@ -32,4 +32,22 @@ public class DBConnection {
 
         return new Result(message, success);
     }
+
+    public static Result login(String email, String password) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        Connection connection = DriverManager.getConnection(url, dbUserName, dbPassword);
+        String query = "{call login(?,?,?,?)}";
+
+        CallableStatement statement = connection.prepareCall(query);
+        statement.setString(1, email);
+        statement.setString(2, password);
+        statement.registerOutParameter(3, Types.VARCHAR);
+        statement.registerOutParameter(4, Types.BOOLEAN);
+        statement.executeUpdate();
+
+        String message = statement.getString(3);
+        boolean success = statement.getBoolean(4);
+
+        return new Result(message, success);
+    }
 }
