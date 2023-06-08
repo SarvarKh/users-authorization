5e1a924586b1668a5acbd15a6e3d71bd52aef6fb
Sarvar Khalimov <khalimovsarvar@gmail.com>
2023-06-08 12:55:25 +0500

    Add Country, District, Region models

---
+++ src/main/java/sarvar/group/modelDao/Country.java
@@ -0,0 +1,15 @@
+package sarvar.group.modelDao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Country {
+    private Integer id;
+    private String name;
+}
